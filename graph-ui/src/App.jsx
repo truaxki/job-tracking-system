b8ce15e479d613178d0a38595be7a1d776663b13
Kirk Truax <157069214+truaxki@users.noreply.github.com>
2024-12-13 14:27:56 -0500

    Add React graph application structure

---
+++ graph-ui/src/App.jsx
@@ -0,0 +1,11 @@
+import { GraphBuilder } from './components/graph-ui/GraphBuilder'
+
+function App() {
+  return (
+    <main className="min-h-screen">
+      <GraphBuilder />
+    </main>
+  )
+}
+
+export default App
